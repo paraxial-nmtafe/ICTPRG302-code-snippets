7b899317c108af9537706bad818d8b32792bd544
Para O'Kelly <Para.OKelly@nmtafe.wa.edu.au>
2025-03-07 09:48:40 +0800

    Added function exercise: baby shark (sorry)

---
+++ exercises/05_01_baby_shark.py
@@ -0,0 +1,70 @@
+# Recall that functions can let you do similar things over and over.
+
+# Using functions, print the lyrics to "Baby Shark"
+
+# For reference, the lyrics are included at the bottom of this file.
+# (I am very sorry if you now have this song in your head.)
+
+
+# A suggestion to start:
+
+def print_song_verse(line_prefix):
+    # The verses of baby shark are very similar to each other
+    # This function could be used to reduce some repetition.
+    # Get rid of this pass statement when you write your own code:
+    pass
+
+# This will print the first verse.  How will you print the others?
+print_song_verse("Baby Shark")
+
+
+# Extension: Could you use loops as well?
+
+###############################################################################
+# Lyrics for reference:
+"""
+Baby Shark, doo-doo, doo-doo, doo-doo
+Baby Shark, doo-doo, doo-doo, doo-doo
+Baby Shark, doo-doo, doo-doo, doo-doo
+Baby Shark
+
+Mommy Shark, doo-doo, doo-doo, doo-doo
+Mommy Shark, doo-doo, doo-doo, doo-doo
+Mommy Shark, doo-doo, doo-doo, doo-doo
+Mommy Shark
+
+Daddy Shark, doo-doo, doo-doo, doo-doo
+Daddy Shark, doo-doo, doo-doo, doo-doo
+Daddy Shark, doo-doo, doo-doo, doo-doo
+Daddy Shark
+
+Grandma Shark, doo-doo, doo-doo, doo-doo
+Grandma Shark, doo-doo, doo-doo, doo-doo
+Grandma Shark, doo-doo, doo-doo, doo-doo
+Grandma Shark
+
+Grandpa Shark, doo-doo, doo-doo, doo-doo
+Grandpa Shark, doo-doo, doo-doo, doo-doo
+Grandpa Shark, doo-doo, doo-doo, doo-doo
+Grandpa Shark
+
+Let's go hunt, doo-doo, doo-doo, doo-doo
+Let's go hunt, doo-doo, doo-doo, doo-doo
+Let's go hunt, doo-doo, doo-doo, doo-doo
+Let's go hunt
+
+Run away, doo-doo, doo-doo, doo-doo
+Run away, doo-doo, doo-doo, doo-doo
+Run away, doo-doo, doo-doo, doo-doo
+Run away (ah!)
+
+Safe at last, doo-doo, doo-doo, doo-doo
+Safe at last, doo-doo, doo-doo, doo-doo
+Safe at last, doo-doo, doo-doo, doo-doo
+Safe at last (phew)
+
+It's the end, doo-doo, doo-doo, doo-doo
+It's the end, doo-doo, doo-doo, doo-doo
+It's the end, doo-doo, doo-doo, doo-doo
+It's the end
+"""
--- exercises/README.md
+++ exercises/README.md
@@ -5,10 +5,10 @@ The class wants more exercises, so I shall attempt to provide.
 
 (For those who know what I mean, please stick to the standard library and don't import anything.)
 
-There are *possible solutions* on the branch called `answers`.  
+There are *possible solutions* on the branch called `answers`.
 These are not full answers, they are just one of many solutions.
 
-If you're keeping up with RIoT, this is good practice to switch branches and keep 
+If you're keeping up with RIoT, this is good practice to switch branches and keep
 an eye on your git log as well 😄.
 
 ## 04: Loops
@@ -16,9 +16,13 @@ an eye on your git log as well 😄.
 - There's a week 4 revision exercise in Blackboard to print multiplication tables
 - I will also add the following: various_pointy_trees.py
 
+## 05: Functions
+- Check blackboard first, and try the calculator exercise.
+- Then: 05_01_baby_shark.py
+
 ## 07, 08: Dictionary & List Exercises
 
 - Have a read through [[./collections.md]]
 - media_list.py just helps you understand that lists can contain dictionaries as elements.
-- star_ratings.py is probably a good extension to the problems we tried during class. 
+- star_ratings.py is probably a good extension to the problems we tried during class.
 
