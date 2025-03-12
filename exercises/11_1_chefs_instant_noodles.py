6dbae6f89a1501171fcf231023426034612d1c0c
Para O'Kelly <Para.OKelly@nmtafe.wa.edu.au>
2025-03-12 16:40:25 +0800

    Revision exercises

---
+++ exercises/11_1_chefs_instant_noodles.py
@@ -0,0 +1,23 @@
+"""
+You are a busy chef and want to know how many customers you can serve, given 
+that you have some number stove burners available, and some number of minutes 
+available to serve them in.
+
+The only thing you cook is instant noodles, each serving of which takes one 
+minute to cook on a stove.
+
+You should take user inputs for stove_count and available_minutes.
+
+Extension 1: You are a busier chef than you thought! You need to input how 
+many stoves and how many minutes you have as a file, from 11_revision_inputs/kitchen.
+
+Extension 2: Your noodles might now take anywhere from 1 to X minutes to cook, where
+"X" is a user input number.
+
+Output a range of number of customers that you might be serving tonight.
+"""
+
+def number_of_customers(stoves, minutes):
+    pass
+
+
---
+++ exercises/11_2_longest_words.py
@@ -0,0 +1,25 @@
+"""
+Write a looping function that counts the length of a string.
+(This is practice, please don't just use "len", even if that is the better way.)
+
+For any given string, please output both it, and the length of it.
+Eg, for input "Python", output "python: 6"
+
+Extension 1:
+Now you have a file of words (see 11_revision_inputs/words).  Write a new file,
+"word_lengths", which contains all of the inputs from "words" but ordered from 
+shortest to longest, in the same format as before.
+
+Extension 2:
+The requirements have changed! You now have a new input (11_revision_inputs/sentences)
+which contains entire sentences!
+
+Please get the longest AND the shortest word from EACH LINE and use that as your
+word list for the above.  Don't include duplicates in the final list.
+
+Exclude words that are only one letter long.
+
+Extension 3: 
+Do some research about regular expressions and ensure that your words from above don't
+have any trailing punctuation.
+"""
